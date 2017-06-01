696d986a0b8095e7ef666e7699ce63561383583e
Biagio Vincenzo Mamone <macbook@MacBook-Air-de-Macbook.local>
2017-06-01 13:14:23 -0300

    Bluetooth connection test

--- android/src/main/java/com/bmamone/RNPagarmeMposModule.java
+++ android/src/main/java/com/bmamone/RNPagarmeMposModule.java
@@ -3,6 +3,9 @@ package com.bmamone;
 
 import android.widget.Toast;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -24,6 +27,25 @@ public class RNPagarmeMposModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void show(String message, int duration) {
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    if (mBluetoothAdapter == null) {
+        // Device does not support Bluetooth
+
+    }
+//    if (!mBluetoothAdapter.isEnabled()) {
+//        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+//    }
+    Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+    if (pairedDevices.size() > 0) {
+      // There are paired devices. Get the name and address of each paired device.
+      for (BluetoothDevice device : pairedDevices) {
+          String deviceName = device.getName();
+          String deviceHardwareAddress = device.getAddress(); // MAC address
+          Toast.makeText(getReactApplicationContext(), deviceName + " " + deviceHardwareAddress, duration).show();
+      }
+    }
     Toast.makeText(getReactApplicationContext(), message, duration).show();
   }
 }
